venkatamutyala/vscode

remove 'assertOneOf' utility in favor of 'isOneOf' + 'assert' pair, update unit tests

src/vs/platform/prompts/test/common/utils/mock.ts
@@ -3,7 +3,8 @@
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
-import { assertOneOf } from '../../../../../base/common/types.js';
+import { assert } from '../../../../../base/common/assert.js';
+import { isOneOf } from '../../../../../base/common/types.js';
 
 /**
  * Mocks an `TObject` with the provided `overrides`.
@@ -26,9 +27,8 @@ export function mockObject<TObject extends Object>(
 		{
 			get: (_target, key: string | number | Symbol) => {
 				// sanity check for the provided `key`
-				assertOneOf(
-					key,
-					keys,
+				assert(
+					isOneOf(key, keys),
 					`The '${key}' is not mocked.`,
 				);
 

tarx-ai/tarx-code-oss

src/vs/platform/prompts/test/common/utils/mock.ts
@@ -3,7 +3,8 @@
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
-import { assertOneOf } from '../../../../../base/common/types.js';
+import { assert } from '../../../../../base/common/assert.js';
+import { isOneOf } from '../../../../../base/common/types.js';
 
 /**
  * Mocks an `TObject` with the provided `overrides`.
@@ -33,10 +34,8 @@ export function mockObject<TObject extends object>(
 				_target: TObject,
 				key: string | number | Symbol,
 			): TObject[T] => {
-
-				assertOneOf(
-					key,
-					keys,
+				assert(
+					isOneOf(KeyedTaskIdentifier, keys),
 					`The '${key}' is not mocked.`,
 				);
 